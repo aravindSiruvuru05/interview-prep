bccfb2c3b41f18f03e6834f5a13dc8db46c7adc1
siruvuruaravind <siruvuruaravind@gmail.com>
2025-04-13 10:32:25 +0530

    recurssion

---
+++ backend/recurssion/index.md
@@ -0,0 +1,66 @@
+
+
+# 🧠 Recursion & Backtracking LeetCode Problems
+
+## Basic Recursion
+- [Factorial](https://leetcode.com/problems/factorial/)
+- [Fibonacci Number](https://leetcode.com/problems/fibonacci-number/)
+- [Reverse a String](https://leetcode.com/problems/reverse-string/)
+
+## Subsets and Combinations
+- [Subsets](https://leetcode.com/problems/subsets/)
+- [Subsets II](https://leetcode.com/problems/subsets-ii/)
+- [Combinations](https://leetcode.com/problems/combinations/)
+- [Combination Sum](https://leetcode.com/problems/combination-sum/)
+- [Combination Sum II](https://leetcode.com/problems/combination-sum-ii/)
+- [Combination Sum III](https://leetcode.com/problems/combination-sum-iii/)
+- [Combination Sum IV](https://leetcode.com/problems/combination-sum-iv/)
+
+## Permutations
+- [Permutations](https://leetcode.com/problems/permutations/)
+- [Permutations II](https://leetcode.com/problems/permutations-ii/)
+- [Next Permutation](https://leetcode.com/problems/next-permutation/)
+
+## Palindrome Partitioning
+- [Palindrome Partitioning](https://leetcode.com/problems/palindrome-partitioning/)
+- [Palindrome Partitioning II](https://leetcode.com/problems/palindrome-partitioning-ii/)
+- [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/)
+
+## N-Queens and Sudoku
+- [N-Queens](https://leetcode.com/problems/n-queens/)
+- [N-Queens II](https://leetcode.com/problems/n-queens-ii/)
+- [Sudoku Solver](https://leetcode.com/problems/sudoku-solver/)
+
+## Word Search and Letter Combinations
+- [Word Search](https://leetcode.com/problems/word-search/)
+- [Word Search II](https://leetcode.com/problems/word-search-ii/)
+- [Letter Combinations of a Phone Number](https://leetcode.com/problems/letter-combinations-of-a-phone-number/)
+- [Generalized Abbreviation](https://leetcode.com/problems/generalized-abbreviation/)
+
+## Unique Paths and Grids
+- [Unique Paths III](https://leetcode.com/problems/unique-paths-iii/)
+- [Robot Room Cleaner](https://leetcode.com/problems/robot-room-cleaner/)
+
+## Generate Parentheses and Expressions
+- [Generate Parentheses](https://leetcode.com/problems/generate-parentheses/)
+- [Expression Add Operators](https://leetcode.com/problems/expression-add-operators/)
+- [Different Ways to Add Parentheses](https://leetcode.com/problems/different-ways-to-add-parentheses/)
+- [Remove Invalid Parentheses](https://leetcode.com/problems/remove-invalid-parentheses/)
+
+## Backtracking Templates and Miscellaneous
+- [Binary Watch](https://leetcode.com/problems/binary-watch/)
+- [Restore IP Addresses](https://leetcode.com/problems/restore-ip-addresses/)
+- [Factor Combinations](https://leetcode.com/problems/factor-combinations/)
+- [Reconstruct Itinerary](https://leetcode.com/problems/reconstruct-itinerary/)
+- [Beautiful Arrangement](https://leetcode.com/problems/beautiful-arrangement/)
+- [Spiral Matrix](https://leetcode.com/problems/spiral-matrix/)
+- [Gray Code](https://leetcode.com/problems/gray-code/)
+
+## Advanced Recursion
+- [Regular Expression Matching](https://leetcode.com/problems/regular-expression-matching/)
+- [Wildcard Matching](https://leetcode.com/problems/wildcard-matching/)
+- [Interleaving String](https://leetcode.com/problems/interleaving-string/)
+
+---
+
+Let me know if you want this as a downloadable `.md` file or categorized by difficulty (easy/medium/hard)!
--- test.py
+++ test.py
@@ -1,38 +1,44 @@
 
-# You have k servers numbered from 0 to k-1 that are being used to handle multiple requests simultaneously. Each server has infinite computational capacity but cannot handle more than one request at a time. The requests are assigned to servers according to a specific algorithm:
+# # You have k servers numbered from 0 to k-1 that are being used to handle multiple requests simultaneously. Each server has infinite computational capacity but cannot handle more than one request at a time. The requests are assigned to servers according to a specific algorithm:
 
-# The ith (0-indexed) request arrives.
-# If all servers are busy, the request is dropped (not handled at all).
-# If the (i % k)th server is available, assign the request to that server.
-# Otherwise, assign the request to the next available server (wrapping around the list of servers and starting from 0 if necessary). For example, if the ith server is busy, try to assign the request to the (i+1)th server, then the (i+2)th server, and so on.
-# You are given a strictly increasing array arrival of positive integers, where arrival[i] represents the arrival time of the ith request, and another array load, where load[i] represents the load of the ith request (the time it takes to complete). Your goal is to find the busiest server(s). A server is considered busiest if it handled the most number of requests successfully among all the servers.
+# # The ith (0-indexed) request arrives.
+# # If all servers are busy, the request is dropped (not handled at all).
+# # If the (i % k)th server is available, assign the request to that server.
+# # Otherwise, assign the request to the next available server (wrapping around the list of servers and starting from 0 if necessary). For example, if the ith server is busy, try to assign the request to the (i+1)th server, then the (i+2)th server, and so on.
+# # You are given a strictly increasing array arrival of positive integers, where arrival[i] represents the arrival time of the ith request, and another array load, where load[i] represents the load of the ith request (the time it takes to complete). Your goal is to find the busiest server(s). A server is considered busiest if it handled the most number of requests successfully among all the servers.
 
 
-from sortedcontainer import SortedList
-busyservers = heap ( next_available_time, server_idx) 
-availableservers = []
+# from sortedcontainer import SortedList
+# busyservers = heap ( next_available_time, server_idx) 
+# availableservers = []
 
-def solve(reqs, load, k):
+# def solve(reqs, load, k):
 
-    n = len(reqs)
-    busy = []
-    availableservers = SortedList(range(k))
+#     n = len(reqs)
+#     busy = []
+#     availableservers = SortedList(range(k))
 
-    for i, (begin, capacity) in enumerate(zip(reqs, load)):
+#     for i, (begin, capacity) in enumerate(zip(reqs, load)):
 
-        while busy and busy[0][0] < begin:
-            _, server_id = heapq.pop(busy)
-            availableservers.add(server_id)
+#         while busy and busy[0][0] < begin:
+#             _, server_id = heapq.pop(busy)
+#             availableservers.add(server_id)
         
-        if not availableservers:
-            continue
+#         if not availableservers:
+#             continue
             
-        server = availableservers.bi_sect(i % k)
-        if server == len(k):
-            server = 0
-        heapq.heappuh(busy, ())
+#         server = availableservers.bi_sect(i % k)
+#         if server == len(k):
+#             server = 0
+#         heapq.heappuh(busy, ())
         
 
 
 
 
+
+for i in {1..5}; do
+  echo "Try #$i"
+  dig grofyy-prod-alb-1233227423.us-east-2.elb.amazonaws.com +stats | grep "Query time"
+  sleep 2
+done
